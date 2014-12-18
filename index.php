fc5ae79bf50ca33e3ccef508b9621fef748dae43
Francisco Baila <francisco_baila@hotmail.com>
2014-12-18 20:12:23 +0000

    first page

--- index.php
+++ index.php
@@ -20,12 +20,24 @@ require('core/functions.php');
         ?>
       </div>
 
-      <div id="initial-message" class="at-center text absolute">
+      <div id="initial-message" class="text absolute line">
         <p>Hi, <b>wellcome to DEI.</b></p>
       </div>
 
-      <div id="find-message" class="at-center text bold absolute">
+      <div id="find-message" class="text bold absolute line">
         <p>What would you like to find?</p>
+      </div>
+
+      <div id="find-options" class="text absolute line">
+        <p id="opt-1" class="opts">— a professor</p>
+        <p id="opt-2" class="opts">— a room</p>
+        <p id="opt-3" class="opts">— a service</p>
+      </div>
+
+      <div id="search" class="search-bar">
+        <form action="demo_form.asp">
+          <input class="text search input" id="text-search" type="text" placeholder="search" name="fname">
+        </form>
       </div>
 
     </div>
--- js/functions.js
+++ js/functions.js
@@ -5,14 +5,6 @@ var app = {
 
 $(document).ready(function() {
   app.init();
-
-  $("body").click(function() {
-    $("#initial-message").addClass('move-top');
-    $("#initial-message").fadeOut(600);
-    setTimeout(function() {
-      $("#find-message").fadeIn(300);
-    }, 600);
-  });
 })
 
 app.init = function() {
@@ -22,5 +14,26 @@ app.init = function() {
 
 app.events = function() {
   var self = this;
+  $("body").click(function() {
+    $("#initial-message").addClass('move-top');
+    $("#initial-message").fadeOut(600);
 
+    setTimeout(function() {
+      $("#find-message").fadeIn(300);
+    }, 600);
+
+    setTimeout(function() {
+      $("#opt-1").fadeIn(300);
+    }, 900);
+    setTimeout(function() {
+      $("#opt-2").fadeIn(300);
+    }, 1100);
+    setTimeout(function() {
+      $("#opt-3").fadeIn(300);
+    }, 1300);
+
+    setTimeout(function() {
+      $("#search").fadeIn(300);
+    }, 1900);
+  });
 }
