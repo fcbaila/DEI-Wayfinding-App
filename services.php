28e98335fecfe7637ab108f7f55316a6feacf8e6
Francisco Baila <francisco_baila@hotmail.com>
2014-12-27 18:48:35 +0000

    services page

--- services.php
+++ services.php
@@ -32,7 +32,7 @@ require('core/functions.php');
           <p>Services:</p>
         </div>
 
-        <div id="professors-list" class="text bold absolute grid-h-3 grid-2">
+        <div id="services-list" class="text bold absolute grid-h-3 grid-2">
           <br>
           <p>GAPI — Gabinete de Apoio a Projectos / Mobilidade;</p>
           <p>Helpdesk;</p>
@@ -41,57 +41,6 @@ require('core/functions.php');
           <p>Bar CISUC;</p>
           <p>Bar;</p>
           <p>WC.</p>
-        </div>
-
-        <div id="search-professors" class="search-bar grid-14 grid-h-2 absolute">
-          <form action="demo_form.asp">
-            <input class="text search input" id="text-search" type="text" placeholder="search" name="fname">
-          </form>
-        </div>
-
-        <div id="professors-filter" class="text grid-14 grid-h-3 absolute">
-          <p id="inside-p-filter">
-            <div class="key" id="a">A</div>
-            <div class="key" id="b">B</div>
-            <div class="key" id="c">C</div>
-            <div class="key" id="d">D</div>
-            <div class="key" id="e">E</div>
-            <div class="key" id="f">F</div>
-            <div class="key" id="g">G</div>
-            <div class="key" id="h">H</div>
-            <div class="key" id="i">I</div>
-            <div class="key" id="j">J</div>
-            <div class="key" id="k">K</div>
-            <div class="key" id="l">L</div>
-            <div class="key" id="m">M</div>
-            <div class="key" id="n">N</div>
-            <div class="key" id="o">O</div>
-            <div class="key" id="p">P</div>
-            <div class="key" id="q">Q</div>
-            <div class="key" id="r">R</div>
-            <div class="key" id="s">S</div>
-            <div class="key" id="t">T</div>
-            <div class="key" id="u">U</div>
-            <div class="key" id="v">V</div>
-            <div class="key" id="x">X</div>
-            <div class="key" id="y">Y</div>
-            <div class="key" id="w">W</div>
-            <div class="key" id="z">Z</div>
-            <div class="key-2" id="space">" "</div>
-            <div class="key-2" id="back"><—</div>
-            <div id="clean">clean</div>
-          </p>
-        </div>
-        <div id="professors-filter-2" class="text grid-14 grid-h-3 absolute">
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <p class="button underline" id="engineer">engineering</p>
-          <p class="button underline" id="designer">design</p>
         </div>
 
         <div id="clock" class="close text absolute grid-16 grid-h-1 button">
