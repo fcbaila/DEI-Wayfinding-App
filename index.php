b2f5b212e6ac44a229d64137cbacacbe4474961e
Francisco Baila <francisco_baila@hotmail.com>
2015-01-01 23:31:37 +0000

    page frame

--- index.php
+++ index.php
@@ -14,35 +14,33 @@ require('core/functions.php');
 
       <div id="grid_container">
         <?php
-        for($i = 1; $i <= 22; $i++) {
+        for($i = 1; $i <= 40; $i++) {
           echo '<div class="grid"></div>';
         }
         ?>
-
         <?php
-        for($i = 1; $i <= 17; $i++) {
+        for($i = 1; $i <= 32; $i++) {
           echo '<div class="grid-h"></div>';
         }
         ?>
       </div>
+      <div id="frame" class="absolute">
+        <div id="frame-top" class="absolute"></div>
+        <div id="frame-right" class="absolute"></div>
+        <div id="frame-bottom" class="absolute"></div>
+        <div id="frame-left" class="absolute"></div>
+      </div>
 
       <div id="page_container" class="full-page">
-
-
-
-        <div id="initial-message" class="text absolute grid-2 grid-h-7">
+        <div id="initial-message" class="text absolute grid-4 grid-h-10 type-48">
           <p>Hi, <b>welcome to DEI.</b></p>
           <p>Department of Informatics Engineering<br>- University of Coimbra</p>
         </div>
 
-        <div id="initial-instruction" class="text absolute grid-2 grid-h-12">
+        <div id="initial-instruction" class="text absolute grid-8 grid-h-20 type-32">
           <p>touch to start</p>
           <img class="img-touch" src="./css/images/touch.png" />
         </div>
-
-        <div id="clock" class="close text absolute grid-16 grid-h-1 button" style="display:none;">
-        </div>
-
       </div>
 
     </div>
