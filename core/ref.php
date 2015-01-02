e0b75224a7fbb5886b03fcb67d0f81b12f311aef
Francisco Baila <francisco_baila@hotmail.com>
2015-01-02 23:15:00 +0000

    favicon

--- core/ref.php
+++ core/ref.php
@@ -4,6 +4,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>DEI-WAYFINDING</title>
 
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
 <link href="./css/style.css" rel="stylesheet" type="text/css">
 <link href="./css/grid.css" rel="stylesheet" type="text/css">
 <link href="./css/style-2.css" rel="stylesheet" type="text/css">
