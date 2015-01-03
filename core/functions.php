ddbb55f253c262aae997782205471002e5f56319
Bruno Barbosa <brunobarbosa@MBP-de-Bruno-3.lan>
2015-01-03 06:16:16 +0000

    Whoops, deleted functions

--- core/functions.php
+++ core/functions.php
@@ -1,4 +1,43 @@
 <?php
+
+function getTeachers() {
+  $teacher = mysql_query("SELECT * from teachers ORDER BY teacher_name ASC");
+  if (!$teacher || mysql_num_rows($teacher) == 0) {
+    echo 'There are no teachers available at the moment.';
+  } else {
+    while($fetch_teachers = mysql_fetch_object($teacher)){
+      echo '<p class="teacher_id teacher_'.$fetch_teachers->teacher_id.'" teacher_filter="'.$fetch_teachers->teacher_course.'" teacher_name="'.$fetch_teachers->teacher_name.'">· '.$fetch_teachers->teacher_name.'</p>';
+    }
+  }
+}
+
+function getRooms() {
+  $room = mysql_query("SELECT * from rooms ORDER BY room_block ASC, room_floor ASC, room_number ASC");
+  if (!$room || mysql_num_rows($room) == 0) {
+    echo 'There are no rooms available at the moment.';
+  } else {
+    while($fetch_rooms = mysql_fetch_object($room)){
+      echo '<p class="room_id '.$fetch_rooms->room_type.' room_'.$fetch_rooms->room_id.'" room_block="'.$fetch_rooms->room_block.'" room_floor="'.$fetch_rooms->room_floor.'"> · '.$fetch_rooms->room_block.$fetch_rooms->room_floor.'.'.$fetch_rooms->room_number.'  '.$fetch_rooms->room_name.'</p>';
+    }
+  }
+}
+
+function showFrontMap() {
+  $count = 0;
+  for ($i = 1; $i <= 8; $i++) {
+    for($j = 1; $j <= 18; $j++) {
+      $count = $count+1;
+      $query = mysql_query("SELECT * FROM map_slots_front WHERE map_x = '$j' AND map_y = '$i'");
+      if (!$query || mysql_num_rows($query) == 0) {
+        echo '<div class="mapa_slot slot_'.$count.'"><img src="css/images/icons/icon_empty.png"></div>';
+      } else {
+        $fetch_query = mysql_fetch_object($query);
+        echo '<div class="mapa_slot slot_'.$count.'"><img src="'.$fetch_query->slot_image.'"></div>';
+      }
+    }
+  }
+}
+
 function showMap($floor) {
   $count = 0;
   $tooltip = 'tooltipsed';
@@ -61,3 +100,5 @@ function showMap($floor) {
     }
   }
 }
+
+?>
